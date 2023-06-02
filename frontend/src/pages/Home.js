7766f75149c16b5c0cc377dd9aebd549cd49f5f3
Matheus-Du <duncanmatheus905@gmail.com>
2023-06-02 10:41:09 -0400

    Setup routing and homepage.

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+
+	return (
+		<div className="Home">
+		<header className="Home-header">
+			<p>
+			Matheus Duncan
+			</p>
+		</header>
+		</div>
+	);
+}
