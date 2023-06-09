d25d370d042bb00bab4414e12fd5dc2424129fb0
Matheus-Du <duncanmatheus905@gmail.com>
2023-06-09 15:52:09 -0400

    Updated routing for single-page layout.

--- frontend/src/components/Menubar.js
+++ frontend/src/components/Menubar.js
@@ -5,16 +5,16 @@ export default function Menubar() {
 
     const navigate = (page) => {
         return function() {
-            window.location.href = "/" + page;
+            window.location.href = page;
         }
     };
 
     return (
         <div className="menubar">
-            <button onClick={ navigate("") } className="menubar-button">Home</button>
-            <button onClick={ navigate("about") } className="menubar-button">About</button>
-            <button onClick={ navigate("blog") } className="menubar-button">Blog</button>
-            <button onClick={ navigate("projects") } className="menubar-button">Projects</button>
+            <button onClick={ navigate("#home") } className="menubar-button">Matheus Duncan</button>
+            <button onClick={ navigate("#about") } className="menubar-button">About</button>
+            <button onClick={ navigate("#projects") } className="menubar-button">Projects</button>
+            <button onClick={ navigate("#blog") } className="menubar-button">Blog</button>
         </div>
     );
 }
