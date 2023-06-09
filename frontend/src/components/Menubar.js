e486aaca76c9f4655a405daa8fb97e17120f084f
Matheus-Du <duncanmatheus905@gmail.com>
2023-06-08 21:20:44 -0400

    Added manubar with basic nav routing.

--- frontend/src/components/Menubar.js
+++ frontend/src/components/Menubar.js
@@ -2,17 +2,19 @@ import React from "react";
 import '../static/css/Menubar.css'
 
 export default function Menubar() {
+
+    const navigate = (page) => {
+        return function() {
+            window.location.href = "/" + page;
+        }
+    };
+
     return (
         <div className="menubar">
-            <div className="menubar-item">
-                <a href="/">Home</a>
-            </div>
-            <div className="menubar-item">
-                <a href="/about">About</a>
-            </div>
-            <div className="menubar-item">
-                <a href="/contact">Contact</a>
-            </div>
+            <button onClick={ navigate("") } className="menubar-button">Home</button>
+            <button onClick={ navigate("about") } className="menubar-button">About</button>
+            <button onClick={ navigate("blog") } className="menubar-button">Blog</button>
+            <button onClick={ navigate("projects") } className="menubar-button">Projects</button>
         </div>
     );
 }
