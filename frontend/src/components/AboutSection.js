3e6256c95b966f9b6c1cab28e504144a41035372
Matheus-Du <duncanmatheus905@gmail.com>
2023-06-08 23:00:59 -0400

    Added detailed profile info.

--- frontend/src/components/AboutSection.js
+++ frontend/src/components/AboutSection.js
@@ -1,15 +1,26 @@
 import React from "react";
 import '../static/css/AboutSection.css'
+import '../static/media/profile.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export default function AboutSection() {
+
     return (
         <div className="main">
             <div className="profile">
-                <h3>Matheus</h3>
+                <img src={require('../static/media/profile.jpg')} alt="Profile" />
+                <h4>Matheus Duncan</h4>
             </div>
             <div className="links">
-                <a href="https://www.linkedin.com/in/matheus-duncan/" target="_blank" rel="noreferrer">LinkedIn</a>
-			    <a href="https://github.com/Matheus-Du" target="_blank" rel="noreferrer">GitHub</a>
+                <div className="link">
+                    <FontAwesomeIcon icon={faGithub} />
+                    <a href="https://www.linkedin.com/in/matheus-duncan/" target="_blank" rel="noreferrer">LinkedIn</a>
+                </div>
+                <div className="link">
+                    <FontAwesomeIcon icon={faLinkedin} />
+			        <a href="https://github.com/Matheus-Du" target="_blank" rel="noreferrer">GitHub</a>
+                </div>
             </div>
         </div>
     );
